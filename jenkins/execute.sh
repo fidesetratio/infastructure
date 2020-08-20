6ed7f424d3522f729440d40d9576456d49bdede8
fidesetratio <patartimotiustambunan@gmail.com>
2020-08-20 18:43:25 +0700

    new execute command for jenkins

--- jenkins/execute.sh
+++ jenkins/execute.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 echo "Execute"
-mkdir jenkinshome
-docker run --detach \
-	   --publish 8082:8080 \
-	   --volume jenkinshome:/var/jenkins_home \
-	   --name jenkins-local \
-	   jenkinsmsig:1.0
+FOLDER=$(pwd)/jenkinshome
+rm -rf $FOLDER
+mkdir $FOLDER
 
+TMP=$(docker stop jenkins-local)
+TMP=$(docker rm jenkins-local)
+TMP=$(docker run --detach \
+	   --publish 8082:8080 \
+	   --volume $FOLDER:/var/jenkins_home \
+	   --name jenkins-local \
+	   --restart always \
+	   jenkinsmsig:1.0)
+
+sleep 10
+echo "Getting exec"
+
+SECRET=$(docker exec jenkins-local cat var/jenkins_home/secrets/initialAdminPassword)
+
+
+echo "Please use this secret  : $SECRET"
