3603446a73fb3e96f90e2153f91d7a4e3ded25cc
fidesetratio <patartimotiustambunan@gmail.com>
2020-08-30 11:33:11 +0700

    tambah untuk docker compose instalation

--- docker-compose/execute.sh
+++ docker-compose/execute.sh
@@ -4,4 +4,20 @@ echo "Install Docker Compose"
 echo "Patar timotius"
 
 
+DOCKERCOMPOSE=/usr/local/bin/docker-compose
 
+if [ -f "$DOCKERCOMPOSE" ]
+then
+	echo "ada ini need to remove it first"
+	sudo rm -f $DOCKERCOMPOSE
+fi
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo usermod -aG docker $USER
+
+docker-compose --version
